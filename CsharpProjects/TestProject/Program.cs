db9e9e479496f8a044dec958a1563306aa43790d
sebastian lapt <smoreira11@hotmail.com>
2025-06-23 08:12:07 +0900

    add

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,48 +1,18 @@
-﻿
-int Totales;
-int i = 0;
-
+﻿string? readResult;
+bool validEntry = false;
+Console.WriteLine("Enter a string containing at least three characters:");
 do
 {
-    Console.WriteLine("\n  Enter an integer value between 5 and 10  :");
-    
-    
-    if (!int.TryParse(Console.ReadLine(), out Totales))
+    readResult = Console.ReadLine();
+    if (readResult != null)
     {
-        Console.WriteLine("Error: Ingrese un número válido entre el 5 y el 10");
-        continue;
+        if (readResult.Length >= 3)
+        {
+            validEntry = true;
+        }
+        else
+        {
+            Console.WriteLine("Your input is invalid, please try again.");
+        }
     }
-    
-    switch (Totales)
-    {     
-       
-        case 1: 
-            
-            Console.WriteLine($" tu numero es inferior a 5 {i}");
-            break;
-        case 2: 
-               if (i > 0) 
-            {
-                
-                Console.WriteLine($"Editado. Total: {i}");
-            }
-            else
-            {
-                Console.WriteLine("No hay elementos para editar");
-            }
-            break;
-             
-        case 3: 
-            Console.WriteLine($"Repitiendo... Contador actual: {i}");
-            break;
-        case 4: 
-            Console.WriteLine("Saliendo del programa...");
-            break;
-        default:
-            Console.WriteLine("Opción no válida numero entre el 5 y 10   ");
-            break;
-            
-    }
-} while  ( Totales != 4);
-
-Console.WriteLine($"Resultados finales - Contador: {i}, Monstruos: ");
+} while (validEntry == false);
